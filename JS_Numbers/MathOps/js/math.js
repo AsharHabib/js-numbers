d3002b7450c8f209bfe60a15f36c3c584b50315e
Ashar Habib <ashar_habib@hotmail.com>
2020-07-31 00:23:40 -0500

    Primitive and math operations

---
+++ JS_Numbers/MathOps/js/math.js
@@ -0,0 +1,3 @@
+const secsinmin = 60, minsinhr = 60, hrsinday = 24, daysinweek = 7, weeksinyr = 52;
+const secsinday = secsinmin * minsinhr * hrsinday;
+console.log(`there r ${secsinday} seconds in a day`);
---
+++ PrimitiveOps/js/string-to-number.js
@@ -0,0 +1,5 @@
+const HTMLBadges = prompt('How many HTML badges do you have?');
+const CSSBadges = prompt('How many CSS badges do you have?');
+
+const totalBadges = parseFloat(HTMLBadges) +parseFloat(CSSBadges);
+console.log(totalBadges);
